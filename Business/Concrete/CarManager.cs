6990c08f735ca5c1a4ca051fe61a919a26f2cffe
zekishak <zekishakdgny38@gmail.com>
2022-07-18 13:30:19 +0300

    second commit(18.07.2022)

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -16,9 +16,34 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        public void add(Car car)
+        {
+            if (car.DailyPrice > 0 && car.Description.Length > 2)
+            {
+               _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Lütfen tekrar giriniz");
+            }
+            
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
         }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(p=>p.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(p=>p.ColorId==colorId);
+        }
+
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            using (RentCarContext rentCarContext = new RentCarContext())
+            {
+                var addedEntity = rentCarContext.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                rentCarContext.SaveChanges();
+            }
+        }
+
+        public void Delete(Car entity)
+        {
+            using (RentCarContext rentCarContext = new RentCarContext())
+            {
+                var deletedEntity = rentCarContext.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                rentCarContext.SaveChanges();
+            }
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            using (RentCarContext rentCarContext = new RentCarContext())
+            {
+                return filter == null ? rentCarContext.Set<Car>().ToList() : rentCarContext.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Car entity)
+        {
+            using (RentCarContext rentCarContext = new RentCarContext())
+            {
+                var uptadedEntity = rentCarContext.Entry(entity);
+                uptadedEntity.State = EntityState.Modified;
+                rentCarContext.SaveChanges();
+            }
+        }
+    }
+}
